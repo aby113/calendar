5aad51f80ccaa48f28377676b664cfc694f045b2
aby113 <aby113@naver.com>
2017-09-15 16:37:22 +0900

    입력받은 월의 최대일수

--- src/calendar/Calendar.java
+++ src/calendar/Calendar.java
@@ -1,5 +1,7 @@
 package calendar;
 
+import java.util.Scanner;
+
 public class Calendar {
 
 	public static void main(String[] args) {
@@ -9,8 +11,32 @@ public class Calendar {
 		System.out.println("8 9 10 11 12 13 14");
 		System.out.println("15 16 17 18 19 20 21");
 		System.out.println("22 23 24 25 26 27 28");
+
+		System.out.println("원하는 달을 입력하세요");
+		int inputMonth = new Scanner(System.in).nextInt();
+		int[] maxMonDay = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+//		System.out.printf("해당 %d 달 최대 일수는 %d 입니다", inputMonth, maxMonDay[inputMonth - 1]);
+		System.out.printf("해당 %d 월 최대 일수는 %d일 입니다", inputMonth, getMonthMaxDay(inputMonth));
+
 	}
-	
-	
-	
+
+	public static int getMonthMaxDay(int i) {
+		int maxDay = 0;
+		switch(i){
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			maxDay = 30;
+			break;
+		case 2:
+			maxDay = 28;
+			break;
+		default:
+			maxDay = 31;
+		}
+		return maxDay;
+	}
+
 }
---
+++ src/calendar/Sum.java
@@ -0,0 +1,19 @@
+package calendar;
+
+import java.util.Scanner;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		
+		System.out.println("숫자 2개를 입력해주세요");
+		int x = new Scanner(System.in).nextInt();
+		int y = new Scanner(System.in).nextInt();
+		System.out.println("두 수의 합은 " + (x + y) + "입니다");
+		
+	}
+	
+	
+	
+	
+}
